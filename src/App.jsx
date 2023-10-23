8f2e4a7e8acc0b4bfb75416215fd99ace4e6030c
VPontis15 <94546957+VPontis15@users.noreply.github.com>
2023-10-23 18:56:22 +0300

    create Pages and add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./Pages/Product";
+import Pricing from "./Pages/Pricing";
+import Homepage from "./Pages/Homepage";
+import PageNotFound from "./Pages/PageNotFound";
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
